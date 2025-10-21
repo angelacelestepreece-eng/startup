6a1932d8d4bb34e26a8f8078471b971c68f9b406
Angela Preece <angela.celeste.preece@gmail.com>
2025-10-20 20:21:11 -0600

    Fixing Injecting the routed component step.

--- src/app.jsx
+++ src/app.jsx
@@ -13,42 +13,42 @@ export default function App() {
         <div className="pink-bg text-dark">
         <header>
         <nav>
-            <a class="navbar-brand" href="#">GroupGoal</a>
-            <menu class="navbar-nav">
-            <li class="nav-item">
+            <a className="navbar-brand" href="#">GroupGoal</a>
+            <ul className="navbar-nav">
+            <li className="nav-item">
                 <NavLink className='nav-link' to='login'>Login</NavLink>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                 <NavLink className='nav-link' to='dashboard'>Dashboard</NavLink>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                 <NavLink className='nav-link' to='progress'>Progress</NavLink>
             </li>
-            <li class="nav-item">
+            <li className="nav-item">
                 <NavLink className='nav-link' to='about'>About</NavLink>
             </li>
-            </menu>
+            </ul>
         </nav>
         </header>
 
         <Routes>
             <Route path='/' element={<Login />} exact />
-            <Route path='/play' element={<Play />} />
-            <Route path='/scores' element={<Scores />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/progress' element={<Progress />} />
             <Route path='/about' element={<About />} />
             <Route path='*' element={<NotFound />} />
         </Routes>
 
         <footer class="pink-bg text-white-50">
-        <div class="container-fluid">
-            <a class="text-reset" href="https://github.com/angelacelestepreece-eng/startup.git">My GitHub</a>
-        </div>
+            <div class="container-fluid">
+                <a class="text-reset" href="https://github.com/angelacelestepreece-eng/startup.git">My GitHub</a>
+            </div>
         </footer>
         </div>
     </BrowserRouter>
   );
 
-  function NotFound() {
-    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
-  }
+    function NotFound() {
+        return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+    }
 }
