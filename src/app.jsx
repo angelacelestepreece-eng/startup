ae53e740fb0dba7b28ace8f6218fc16ec8be4b3b
Angela Preece <angela.celeste.preece@gmail.com>
2025-10-20 19:53:12 -0600

    Create App Component

---
+++ src/app.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+  return (
+    <div className="pink-bg text-dark">
+      <header>
+      <nav>
+        <a class="navbar-brand" href="#">GroupGoal</a>
+        <menu class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="dashboard.html">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="progress.html">Progress</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+        </menu>
+      </nav>
+    </header>
+
+      <main>App components go here</main>
+
+      <footer class="pink-bg text-white-50">
+      <div class="container-fluid">
+        <a class="text-reset" href="https://github.com/angelacelestepreece-eng/startup.git">My GitHub</a>
+      </div>
+    </footer>
+    </div>
+  );
+}
