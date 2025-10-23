0b7fca854869cdc9911b3a596c167947390de7fd
Angela Preece <angela.celeste.preece@gmail.com>
2025-10-22 21:26:13 -0600

    Adding react elements to login page

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -4,23 +4,45 @@ import { Form, Button, InputGroup } from 'react-bootstrap';
 
 
 export function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [status, setStatus] = useState('');
+
+  useEffect(() => {
+    const savedEmail = localStorage.getItem('groupgoal-email');
+    if (savedEmail) setEmail(savedEmail);
+  }, []);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    localStorage.setItem('groupgoal-email', email);
+    setStatus('Logged in as ${email}$');
+  };
+
+  const handleCreate = (e) => {
+    e.preventDefault();
+    localStorage.setItem('groupgoal-email', email);
+  }
+ 
+
   return (
     <main className="container-fluid cream-bg text-center">
       <div>
         <h1 className="outlined-text">Welcome to GroupGoal</h1>
         <h5 className="outlined-text">Made by Angela Preece</h5>
-        <Form method="get" action="progress.html">
+        <Form>
           <InputGroup className="input-group mb-3">
             <InputGroup.Text>@</InputGroup.Text>
-            <Form.Control type="text" placeholder="your@email.com" />
+            <Form.Control type="text" placeholder="your@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
           </InputGroup>
           <InputGroup className="mb-3">
             <InputGroup.Text>🔒</InputGroup.Text>
-            <Form.Control type="password" placeholder="password" />
+            <Form.Control type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
           </InputGroup>
-          <Button type="submit" className="pink-btn me-2">Login</Button>
-          <Button type="submit" className="pink-btn">Create</Button>
+          <Button onClick={handleLogin} className="pink-btn me-2">Login</Button>
+          <Button onClick={handleCreate} className="pink-btn">Create</Button>
         </Form>
+        {status && <p className="mt-3 text-success">{status}</p>}
       </div>
     </main>
   );
