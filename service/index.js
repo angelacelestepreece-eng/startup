cb6a84549c71340d3be44e521e643c61ece44f0e
Angela Preece <angela.celeste.preece@gmail.com>
2025-11-13 22:12:06 -0700

    Fixing backend issues.

--- service/index.js
+++ service/index.js
@@ -6,7 +6,7 @@ const app = express();
 const DB = require('./database.js');
 
 const authCookieName = 'token';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
