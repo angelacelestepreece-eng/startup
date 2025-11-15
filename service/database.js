5cd9c8466b26f4136170fb787d68a9d0c50037bc
Angela Preece <angela.celeste.preece@gmail.com>
2025-11-15 13:49:33 -0700

    Still fixing backend issues.

---
+++ service/database.js
@@ -0,0 +1,56 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+
+let db, userCollection, progressCollection;
+
+async function init() {
+  try {
+    await client.connect();
+    db = client.db('startup');
+    userCollection = db.collection('user');
+    progressCollection = db.collection('progress');
+    await db.command({ ping: 1 });
+    console.log(`Connected to database`);
+  } catch (ex) {
+    console.error(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+}
+
+init();
+
+function getUser(email) {
+  return userCollection.findOne({ email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function addProgress(progress) {
+  return progressCollection.insertOne(progress);
+}
+
+function getProgress() {
+  return progressCollection.find().sort({ createdAt: -1 }).limit(10).toArray();
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  addProgress,
+  getProgress,
+};
--- service/index.js
+++ service/index.js
@@ -2,36 +2,17 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
-const path = require('path');
-const { MongoClient } = require('mongodb');
-const dbConfig = require('./dbConfig.json'); // load your config
-
 const app = express();
+const DB = require('./database.js');
+
 const authCookieName = 'token';
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
-
-// Build MongoDB URI using config
-const url = `mongodb+srv://${dbConfig.userName}:${dbConfig.password}@${dbConfig.hostname}/?retryWrites=true&w=majority`;
-const client = new MongoClient(url);
-const dbName = 'myAppDB';
-
-let usersCollection;
-let progressCollection;
-
-async function connectDB() {
-  await client.connect();
-  console.log('Connected to MongoDB');
-  const db = client.db(dbName);
-  usersCollection = db.collection('users');
-  progressCollection = db.collection('progress');
-}
-connectDB().catch(console.error);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 
-var apiRouter = express.Router();
+const apiRouter = express.Router();
 app.use('/api', apiRouter);
 
 apiRouter.post('/auth/create', async (req, res) => {
@@ -47,12 +28,9 @@ apiRouter.post('/auth/create', async (req, res) => {
 apiRouter.post('/auth/login', async (req, res) => {
   const user = await findUser('email', req.body.email);
   if (user && await bcrypt.compare(req.body.password, user.password)) {
-    const newToken = uuid.v4();
-    await usersCollection.updateOne(
-      { email: user.email },
-      { $set: { token: newToken } }
-    );
-    setAuthCookie(res, newToken);
+    user.token = uuid.v4();
+    await DB.updateUser(user);
+    setAuthCookie(res, user.token);
     res.send({ email: user.email });
     return;
   }
@@ -62,10 +40,8 @@ apiRouter.post('/auth/login', async (req, res) => {
 apiRouter.delete('/auth/logout', async (req, res) => {
   const user = await findUser('token', req.cookies[authCookieName]);
   if (user) {
-    await usersCollection.updateOne(
-      { email: user.email },
-      { $unset: { token: "" } }
-    );
+    delete user.token;
+    await DB.updateUser(user);
   }
   res.clearCookie(authCookieName);
   res.status(204).end();
@@ -81,7 +57,7 @@ const verifyAuth = async (req, res, next) => {
 };
 
 apiRouter.get('/progress', verifyAuth, async (req, res) => {
-  const items = await progressCollection.find().sort({ _id: -1 }).toArray();
+  const items = await DB.getProgress();
   res.send(items);
 });
 
@@ -89,21 +65,29 @@ apiRouter.post('/progress', verifyAuth, async (req, res) => {
   if (!req.body.msg) {
     return res.status(400).send({ msg: 'Missing message' });
   }
-  await progressCollection.insertOne({ msg: req.body.msg, createdAt: new Date() });
-  const items = await progressCollection.find().sort({ _id: -1 }).toArray();
+  await DB.addProgress({ msg: req.body.msg, createdAt: new Date() });
+  const items = await DB.getProgress();
   res.send(items);
+});
+
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
 });
 
 async function createUser(email, password) {
   const passwordHash = await bcrypt.hash(password, 10);
   const user = { email, password: passwordHash, token: uuid.v4() };
-  await usersCollection.insertOne(user);
+  await DB.addUser(user);
   return user;
 }
 
 async function findUser(field, value) {
   if (!value) return null;
-  return await usersCollection.findOne({ [field]: value });
+  return field === 'token' ? DB.getUserByToken(value) : DB.getUser(value);
 }
 
 function setAuthCookie(res, authToken) {
@@ -115,14 +99,6 @@ function setAuthCookie(res, authToken) {
   });
 }
 
-app.use(function (err, req, res, next) {
-  res.status(500).send({ type: err.name, message: err.message });
-});
-
-app.use((_req, res) => {
-  res.sendFile('index.html', { root: 'public' });
-});
-
-app.listen(port, () => {
+const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
