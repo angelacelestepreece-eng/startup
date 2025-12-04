d55030b0ffa6a5dedf7ff7920b3b1b349ff7271f
Angela Preece <angela.celeste.preece@gmail.com>
2025-12-03 18:50:43 -0700

    Adding purpose and how to use on the about page.

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -23,12 +23,21 @@ export function About() {
     <main className="container-fluid cream-bg text-center">
       <section id="app-description">
         <h2>Purpose</h2>
-        <p>This is where I will describe the purpose of the app.</p>
+        <p>The Group Goal application is for users to 
+           stay consistent and motivated towards achieving their goals 
+           as a team. Users can set personalized goals for their group 
+           and keep each other accountable. As users add progress towards goals, 
+           the update is shared live with the group. With everyone 
+           working together, goals can finally become reality.</p>
       </section>
 
       <section id="app-use">
         <h2>How to use GroupGoal</h2>
-        <p>This is where I will describe how users actually use the app.</p>
+        <p>Step 1: Think of a goal you would like to work towards!</p>
+        <p>Step 2: Add your goal to the Dashboard.</p>
+        <p>Step 3: Update Progress as you work toward your goal.</p>
+        <p>Step 4: Watch as others in your group contribute to the goals on the Progress page.</p>
+        <p>Step 5: Complete your goal and repeat!</p>
       </section>
 
       <div id="picture" className="picture-box">
